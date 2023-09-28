13bfb5726e6889756d40181ceccc29956952bd85
AmarilloMatias <78113499+AmarilloMatias@users.noreply.github.com>
2023-09-28 18:37:50 -0300

    Add files via upload

---
+++ Para entregar/funcion.js
@@ -0,0 +1,20 @@
+
+
+
+document.getElementById("boton").onclick = function ()
+{
+    document.getElementById("demo").innerHTML = "Todo lo que necesitas saber de mi"
+}
+
+document.getElementById("boton_color").onclick = function ()
+
+{
+    document.body.style.backgroundColor = "#FF0000";
+}
+
+document.getElementById("boton_default").onclick = function ()
+
+{
+    document.body.style.backgroundColor = "white";
+}
+
